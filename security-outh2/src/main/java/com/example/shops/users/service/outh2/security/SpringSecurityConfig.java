7e12e8514e7fb85e92ad54b1cedf7fc2782fd095
Rafael Farfan| <rafaeldanielfarfan@gmail.com>
2023-04-05 11:48:07 -0500

    event handler, succes and error

--- security-outh2/src/main/java/com/example/shops/users/service/outh2/security/SpringSecurityConfig.java
+++ security-outh2/src/main/java/com/example/shops/users/service/outh2/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.shops.users.service.outh2.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService avatarService;
+	
+	@Autowired
+	private AuthenticationEventPublisher eventPublisher;
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
@@ -24,7 +28,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override	
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.avatarService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(this.avatarService).passwordEncoder(passwordEncoder())
+		.and().authenticationEventPublisher(eventPublisher);
 	}
 
 	@Override
